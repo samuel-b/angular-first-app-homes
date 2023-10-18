cc217ea5f4ebb59abb739f1ec1e54b5de3e850a8
Samuel Brooker - WL <samuelbrookerwl@Samuels-MacBook-Pro-WL.local>
2023-10-19 10:09:36 +1100

    changed data-source to jsoon bin and seperated template from components

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -7,21 +7,8 @@ import { RouterModule } from '@angular/router';
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule, RouterModule],
-  template: `
-    <section class="listing">
-      <img
-        class="listing-photo"
-        [src]="housingLocation.photo"
-        alt="Exterior photo of {{ housingLocation.name }}"
-      />
-      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-      <p class="listing-location">
-        {{ housingLocation.city }}, {{ housingLocation.state }}
-      </p>
-      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
-    </section>
-  `,
   styleUrls: ['./housing-location.component.css'],
+  templateUrl: 'housing-location.component.html',
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
